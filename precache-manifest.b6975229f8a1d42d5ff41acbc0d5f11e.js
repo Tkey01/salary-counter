19eecf22613a96ee3c2f8e157f8734a3db6eb0e9
tkey01 <forwebmoney5@yandex.ru>
2021-01-28 09:05:05 +0800

    Updates

---
+++ precache-manifest.b6975229f8a1d42d5ff41acbc0d5f11e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0233988782c0f4996f432f1d75abd455",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bdc0cdb793c92b69d17",
+    "url": "/static/css/main.1ea706f6.chunk.css"
+  },
+  {
+    "revision": "f91ebdc0624b6567d21b",
+    "url": "/static/js/2.9322c693.chunk.js"
+  },
+  {
+    "revision": "1b9cc396521eecb2045c787139670fba",
+    "url": "/static/js/2.9322c693.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bdc0cdb793c92b69d17",
+    "url": "/static/js/main.5798b898.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "c8e8807125d3b619df1815d27a70cfa8",
+    "url": "/static/media/close.c8e88071.svg"
+  },
+  {
+    "revision": "fb39fb758edac25a14f468acdf2de634",
+    "url": "/static/media/info.fb39fb75.svg"
+  }
+]);
